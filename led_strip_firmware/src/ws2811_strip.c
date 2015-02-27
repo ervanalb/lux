3c8a223c92b0c0e9a5094cd824aeb0709d174665
Eric Van Albert <eric@van.al>
2015-02-27 10:13:57 -0500

    broken; trying to implement DMA interrupts

--- led_strip_firmware/src/ws2811_strip.c
+++ led_strip_firmware/src/ws2811_strip.c
@@ -3,19 +3,16 @@
 #include "strip.h"
 #include "stm32f0xx.h"
 
-//#define PULSE_PERIOD 610
-//#define PULSE_PERIOD 122
-//#define PULSE_WIDTH_0 18
-//#define PULSE_WIDTH_1 35
-
-#define ONE_HIGH_T 18
-#define ONE_LOW_T 35
-#define ZERO_HIGH_T 35
-#define ZERO_LOW_T 18
+#define PULSE_PERIOD 61
+#define PULSE_WIDTH_0 18
+#define PULSE_WIDTH_1 35
 
 #define STRIP_MEMORY_LENGTH (STRIP_LENGTH*3)
 
+#define PULSE_BUFFER_LENGTH 64
+
 static uint8_t strip_memory[STRIP_MEMORY_LENGTH];
+static uint8_t pulse_buffer[PULSE_BUFFER_LENGTH];
 
 void strip_init()
 {
@@ -23,19 +20,19 @@ void strip_init()
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
     TIM_OCInitTypeDef TIM_OCInitStruct;
     NVIC_InitTypeDef NVIC_InitStruct;
+    DMA_InitTypeDef DMA_InitStructure;
 
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
     // Pulses out to strip
     GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
     GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
     GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOB,&GPIO_InitStruct);
-    //GPIO_PinAFConfig(GPIOB,GPIO_PinSource5,GPIO_AF_1);
+    GPIO_PinAFConfig(GPIOB,GPIO_PinSource5,GPIO_AF_1);
 
-/*
     // TIM3 CH2
     TIM_TimeBaseInitStruct.TIM_Prescaler = 0;
     TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
@@ -56,15 +53,30 @@ void strip_init()
     TIM_OC2Init(TIM3, &TIM_OCInitStruct);
     TIM_CtrlPWMOutputs(TIM3, ENABLE);
     TIM_Cmd(TIM3, ENABLE);
-*/
+
+    // DMA - SPI
+    DMA_DeInit(DMA1_Channel3);
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(TIM3->CCR2));
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)pulse_buffer;
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+    DMA_InitStructure.DMA_BufferSize = PULSE_BUFFER_LENGTH;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    DMA_Init(DMA1_Channel3, &DMA_InitStructure);
+    TIM_DMACmd(TIM3, TIM_DMA_Update, ENABLE);
 
     // NVIC
-    /*
-    NVIC_InitStruct.NVIC_IRQChannel = TIM3_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannel = DMA1_Channel2_3_IRQn;
     NVIC_InitStruct.NVIC_IRQChannelPriority = 0;
     NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStruct);
-    */
+
+    DMA_ITConfig(DMA1_Channel3, DMA_IT_TC | DMA_IT_HT, ENABLE);
 }
 
 static uint8_t* data_pointer;
@@ -87,101 +99,16 @@ void strip_write(uint8_t* rgb_data)
     //TIM3->SR = ~TIM_FLAG_Update;
     //TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
 
-    for(;;)
+    for(int i=0;i<PULSE_BUFFER_LENGTH;i+=2)
     {
-        if(!bit_counter) break;
-
-        bit_counter--;
-
-        if(data_pointer[bit_counter>>3] & 0x80)
-        {
-            GPIOB->BSRR = GPIO_Pin_5;
-            // 1 HIGH
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            GPIOB->BSRR = (GPIO_Pin_5 << 16);
-            // 1 LOW
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-         }
-        else
-        {
-            GPIOB->BSRR = GPIO_Pin_5;
-            // 0 HIGH
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            GPIOB->BSRR = (GPIO_Pin_5 << 16);
-            // 0 LOW
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-            __NOP();
-         }
-        data_pointer[bit_counter>>3] <<= 1;
+        pulse_buffer[i]=PULSE_WIDTH_0;
+        pulse_buffer[i+1]=PULSE_WIDTH_1;
     }
+
+    DMA_ClearFlag(DMA1_FLAG_TC3);
+    DMA_ClearFlag(DMA1_FLAG_HT3);
+    DMA_SetCurrDataCounter(DMA1_Channel3,PULSE_BUFFER_LENGTH);
+    DMA_Cmd(DMA1_Channel3, ENABLE);
 }
 
 uint8_t strip_ready()
@@ -236,4 +163,20 @@ void TIM3_IRQHandler(void)
     TIM3->SR = ~TIM_FLAG_Update;
 }
 */
+
+void DMA1_Channel2_3_IRQHandler(void)
+{
+    DMA_Cmd(DMA1_Channel3, DISABLE);
+    if(DMA1->ISR & DMA1_IT_HT3)
+    {
+        DMA1->IFCR = DMA1_IT_HT3;
+    }
+
+    if(DMA1->ISR & DMA1_IT_TC3)
+    {
+        
+        DMA1->IFCR = DMA1_IT_TC3;
+    }
+}
+
 #endif
