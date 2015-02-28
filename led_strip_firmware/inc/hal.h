610ad1d32b335ec9bd0d71fb5964207f70a74e29
Eric Van Albert <eric@van.al>
2015-02-28 16:21:37 -0500

    first shot at making minimal example for DMA

--- led_strip_firmware/inc/hal.h
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-
-void init();
-void led_on();
-void led_off();
-uint8_t button();
--- led_strip_firmware/src/main.c
+++ led_strip_firmware/src/main.c
@@ -4,70 +4,177 @@
 
 #include "stm32f0xx.h"
 
-const char id[]="WS2811 LED Strip";
-#define ID_SIZE (sizeof(id)-1)
-
-uint8_t match_destination(uint8_t* dest);
-void rx_packet();
-
-// First byte of packet is command
-
-#define CMD_FRAME 0x00
-
 void main()
 {
-    init();
-    strip_init();
-    lux_init();
+    DMA_InitTypeDef DMA_InitStructure;
+    GPIO_InitTypeDef GPIO_InitStruct;
+    USART_InitTypeDef USART_InitStructure;
 
-    lux_fn_match_destination = &match_destination;
-    lux_fn_rx = &rx_packet;
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB |
+        RCC_AHBPeriph_GPIOF | RCC_AHBPeriph_DMA1 | RCC_AHBPeriph_CRC, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_SYSCFG, ENABLE);
+    
+    // LED
+    led_off();
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_1;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOF,&GPIO_InitStruct);
 
-    for(;;)
+    // Button
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOB,&GPIO_InitStruct);
+
+    // USART
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA,&GPIO_InitStruct);
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_1);
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_1);
+
+    GPIO_ResetBits(GPIOA,GPIO_Pin_1); // Serial Receive
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_1;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA,&GPIO_InitStruct);
+
+    USART_DeInit(USART1);
+    USART_StructInit(&USART_InitStructure);
+    USART_InitStructure.USART_BaudRate = 3000000;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART1,&USART_InitStructure);
+    USART_Cmd(USART1,ENABLE);
+
+    // USART DMA
+    DMA_DeInit(DMA1_Channel5);
+    DMA_InitStructure.DMA_BufferSize = SERIAL_BUFFER_SIZE;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)serial_buffer;
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_Low;
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(USART1->RDR);
+    DMA_Init(DMA1_Channel5, &DMA_InitStructure);
+    SYSCFG_DMAChannelRemapConfig(SYSCFG_DMARemap_USART1Rx,ENABLE);
+
+    DMA_DeInit(DMA1_Channel2);
+    tx_DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    tx_DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    tx_DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    tx_DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    tx_DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+    tx_DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    tx_DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+    tx_DMA_InitStructure.DMA_Priority = DMA_Priority_Low;
+    tx_DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(USART1->TDR);
+    // do not init the TX channel, this is done later
+
+    USART_DMACmd(USART1, USART_DMAReq_Rx | USART_DMAReq_Tx, ENABLE);
+
+    // CRC
+    CRC_DeInit();
+    CRC_ReverseOutputDataCmd(ENABLE);
+    CRC_ReverseInputDataSelect(CRC_ReverseInputData_8bits);
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+    TIM_OCInitTypeDef TIM_OCInitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+    DMA_InitTypeDef DMA_InitStructure;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+    // Pulses out to strip
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOB,&GPIO_InitStruct);
+    GPIO_PinAFConfig(GPIOB,GPIO_PinSource5,GPIO_AF_1);
+
+    // TIM3 CH2
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 0;
+    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStruct.TIM_Period = PULSE_PERIOD;
+    TIM_TimeBaseInitStruct.TIM_ClockDivision = 0;
+    TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStruct);
+
+    TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM2;
+    TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStruct.TIM_OutputNState = TIM_OutputNState_Disable;
+    TIM_OCInitStruct.TIM_Pulse = 0;
+    TIM_OCInitStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
+    TIM_OCInitStruct.TIM_OCIdleState = TIM_OCIdleState_Reset;
+    TIM_OCInitStruct.TIM_OCNPolarity = TIM_OCNPolarity_Low;
+    TIM_OCInitStruct.TIM_OCNIdleState = TIM_OCNIdleState_Reset;
+
+    TIM_OC2Init(TIM3, &TIM_OCInitStruct);
+    TIM_CtrlPWMOutputs(TIM3, ENABLE);
+    TIM_Cmd(TIM3, ENABLE);
+
+    // DMA - TIM3_UP
+    DMA_DeInit(DMA1_Channel3);
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(TIM3->CCR2));
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)pulse_buffer;
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+    DMA_InitStructure.DMA_BufferSize = PULSE_BUFFER_LENGTH;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    DMA_Init(DMA1_Channel3, &DMA_InitStructure);
+    TIM_DMACmd(TIM3, TIM_DMA_Update, ENABLE);
+
+    // NVIC
+    NVIC_InitStruct.NVIC_IRQChannel = DMA1_Channel2_3_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStruct);
+
+    DMA_ITConfig(DMA1_Channel3, DMA_IT_TC | DMA_IT_HT, ENABLE);
+
+    DMA_ClearFlag(DMA1_FLAG_TC3);
+    DMA_ClearFlag(DMA1_FLAG_HT3);
+    DMA_SetCurrDataCounter(DMA1_Channel3,PULSE_BUFFER_LENGTH);
+    DMA_Cmd(DMA1_Channel3, ENABLE);
+}
+
+void DMA1_Channel2_3_IRQHandler(void)
+{
+    DMA_Cmd(DMA1_Channel3, DISABLE);
+    if(DMA1->ISR & DMA1_IT_HT3)
     {
-        lux_codec();
+        DMA1->IFCR = DMA1_IT_HT3;
+    }
+
+    if(DMA1->ISR & DMA1_IT_TC3)
+    {
+        
+        DMA1->IFCR = DMA1_IT_TC3;
     }
 }
 
-uint8_t match_destination(uint8_t* dest)
-{
-    return *(uint32_t*)dest == 0xFFFFFFFF;
-}
 
-static void clear_destination()
-{
-    *(uint32_t*)lux_destination = 0;
-}
-
-static void send_id()
-{
-    int i;
-    lux_stop_rx();
-    clear_destination();
-    lux_packet_length = ID_SIZE;
-    for(i=0;i<ID_SIZE;i++)
-    {
-        lux_packet[i]=id[i];
-    }
-    lux_packet_in_memory = 0;
-    lux_start_tx();
-}
-
-void rx_packet()
-{
-    if(lux_packet_length == 0)
-    {
-        send_id();
-    }
-
-    switch(lux_packet[0])
-    {
-        case CMD_FRAME:
-            if(lux_packet_length != 3*STRIP_LENGTH+1) goto skip_frame;
-            if(!strip_ready()) goto skip_frame;
-            strip_write(&lux_packet[1]);
-        skip_frame:
-            lux_packet_in_memory=0;
-            break;
-    }
-}
