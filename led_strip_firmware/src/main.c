ef51bf69d0dd1171740ababc1f4d37f5fe68d44c
Eric Van Albert <eric@van.al>
2015-02-28 20:09:15 -0500

    minimal working example

--- led_strip_firmware/src/main.c
+++ led_strip_firmware/src/main.c
@@ -1,117 +1,21 @@
-#include "hal.h"
-#include "lux.h"
-#include "strip.h"
-
 #include "stm32f0xx.h"
+
+#define PULSE_PERIOD 200
+
+#define PULSE_BUFFER_LENGTH 64
+
+uint8_t pulse_buffer[PULSE_BUFFER_LENGTH];
 
 void main()
 {
     DMA_InitTypeDef DMA_InitStructure;
-    GPIO_InitTypeDef GPIO_InitStruct;
-    USART_InitTypeDef USART_InitStructure;
-
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB |
-        RCC_AHBPeriph_GPIOF | RCC_AHBPeriph_DMA1 | RCC_AHBPeriph_CRC, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_SYSCFG, ENABLE);
-    
-    // LED
-    led_off();
-    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_1;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOF,&GPIO_InitStruct);
-
-    // Button
-    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
-    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-    GPIO_Init(GPIOB,&GPIO_InitStruct);
-
-    // USART
-    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOA,&GPIO_InitStruct);
-    GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_1);
-    GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_1);
-
-    GPIO_ResetBits(GPIOA,GPIO_Pin_1); // Serial Receive
-    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_1;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOA,&GPIO_InitStruct);
-
-    USART_DeInit(USART1);
-    USART_StructInit(&USART_InitStructure);
-    USART_InitStructure.USART_BaudRate = 3000000;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    USART_Init(USART1,&USART_InitStructure);
-    USART_Cmd(USART1,ENABLE);
-
-    // USART DMA
-    DMA_DeInit(DMA1_Channel5);
-    DMA_InitStructure.DMA_BufferSize = SERIAL_BUFFER_SIZE;
-    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
-    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
-    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)serial_buffer;
-    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
-    DMA_InitStructure.DMA_Priority = DMA_Priority_Low;
-    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(USART1->RDR);
-    DMA_Init(DMA1_Channel5, &DMA_InitStructure);
-    SYSCFG_DMAChannelRemapConfig(SYSCFG_DMARemap_USART1Rx,ENABLE);
-
-    DMA_DeInit(DMA1_Channel2);
-    tx_DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-    tx_DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
-    tx_DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    tx_DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-    tx_DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-    tx_DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-    tx_DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
-    tx_DMA_InitStructure.DMA_Priority = DMA_Priority_Low;
-    tx_DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(USART1->TDR);
-    // do not init the TX channel, this is done later
-
-    USART_DMACmd(USART1, USART_DMAReq_Rx | USART_DMAReq_Tx, ENABLE);
-
-    // CRC
-    CRC_DeInit();
-    CRC_ReverseOutputDataCmd(ENABLE);
-    CRC_ReverseInputDataSelect(CRC_ReverseInputData_8bits);
-
-    GPIO_InitTypeDef GPIO_InitStruct;
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
-    TIM_OCInitTypeDef TIM_OCInitStruct;
     NVIC_InitTypeDef NVIC_InitStruct;
-    DMA_InitTypeDef DMA_InitStructure;
 
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-    // Pulses out to strip
-    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-    GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
-    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOB,&GPIO_InitStruct);
-    GPIO_PinAFConfig(GPIOB,GPIO_PinSource5,GPIO_AF_1);
-
-    // TIM3 CH2
+    // TIM3
     TIM_TimeBaseInitStruct.TIM_Prescaler = 0;
     TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
     TIM_TimeBaseInitStruct.TIM_Period = PULSE_PERIOD;
@@ -119,17 +23,6 @@ void main()
     TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0;
     TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStruct);
 
-    TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM2;
-    TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStruct.TIM_OutputNState = TIM_OutputNState_Disable;
-    TIM_OCInitStruct.TIM_Pulse = 0;
-    TIM_OCInitStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
-    TIM_OCInitStruct.TIM_OCIdleState = TIM_OCIdleState_Reset;
-    TIM_OCInitStruct.TIM_OCNPolarity = TIM_OCNPolarity_Low;
-    TIM_OCInitStruct.TIM_OCNIdleState = TIM_OCNIdleState_Reset;
-
-    TIM_OC2Init(TIM3, &TIM_OCInitStruct);
-    TIM_CtrlPWMOutputs(TIM3, ENABLE);
     TIM_Cmd(TIM3, ENABLE);
 
     // DMA - TIM3_UP
@@ -164,17 +57,7 @@ void main()
 
 void DMA1_Channel2_3_IRQHandler(void)
 {
-    DMA_Cmd(DMA1_Channel3, DISABLE);
-    if(DMA1->ISR & DMA1_IT_HT3)
-    {
-        DMA1->IFCR = DMA1_IT_HT3;
-    }
-
-    if(DMA1->ISR & DMA1_IT_TC3)
-    {
-        
-        DMA1->IFCR = DMA1_IT_TC3;
-    }
+    for(;;);
 }
 
 
