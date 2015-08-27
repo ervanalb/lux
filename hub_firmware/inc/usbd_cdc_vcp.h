c4ba74a7680d2d7268e4072dacdb45090cc14f13
Eric Van Albert <eric@van.al>
2015-08-27 00:06:11 -0400

    remove shitty VCP functionality, get closer to raw USB

--- hub_firmware/inc/usbd_cdc_vcp.h
+++ hub_firmware/inc/usbd_cdc_vcp.h
@@ -33,33 +33,17 @@
 #include "usbd_cdc_core.h"
 
 /* Exported typef ------------------------------------------------------------*/
-/* The following structures groups all needed parameters to be configured for the 
-   ComPort. These parameters can be modified on the fly by the host through CDC class
-   command class requests. */
-typedef struct
-{
-  uint32_t bitrate;
-  uint8_t  format;
-  uint8_t  paritytype;
-  uint8_t  datatype;
-}LINE_CODING;
-
 /* Exported constants --------------------------------------------------------*/
 /* The following define is used to route the USART IRQ handler to be used.
    The IRQ handler function is implemented in the usbd_cdc_vcp.c file. */
           
-#define EVAL_COM_IRQHandler            USART2_IRQHandler
-#define USART_IT_PRIO                  0 
-#define USB_IT_PRIO                    1 /* USB priority should be lower than USART
-                                            in order to not loose data reception from
-                                            USART while USB interrupt is being handled*/
-
 #define DEFAULT_CONFIG                  0
 #define OTHER_CONFIG                    1
 
+#define USB_IT_PRIO 1
+
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-void EVAL_COM_IRQHandler(void);
 
 #endif /* __USBD_CDC_VCP_H */
 
